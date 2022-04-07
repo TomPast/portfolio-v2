6f24fa8f58cafde30bab9da89746d2490ad3b5ff
Tom <tom.pastor.etu@univ-lemans.fr>
2022-04-07 19:58:43 +0200

    feat: setup intro part

---
+++ components/IntroPart/IntroPart.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="intro-content">
+    <div class="catchup">
+      <span class="start-text">Hey there! I'm-</span>
+      <h1 class="title">Tom Pastor.</h1>
+    </div>
+
+    <div class="description">
+      <h2 class="title">Frontend Developer.</h2>
+      <p class="text">
+        A software engineer based in France with an interest in frontend.
+      </p>
+    </div>
+
+    <div class="sentences">
+      <p><span class="icon">🚀</span> Open to work</p>
+      <p>
+        <span class="icon">💻</span> Currently working at Spikeelabs as
+        Fullstack developer
+      </p>
+    </div>
+
+    <div class="actions">
+      <BaseButton @click.native="redirectToGitHub"> GitHub </BaseButton>
+      <BaseButton @click.native="redirectToLinkedin"> Linkedin </BaseButton>
+      <BaseButton @click.native="openMail"> Email </BaseButton>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IntroPart',
+  components: {},
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {
+    redirectToGitHub() {
+      window.open('https://github.com/TomPast', '_blank')
+    },
+    redirectToLinkedin() {
+      window.open('https://www.linkedin.com/in/tom-pastor/', '_blank')
+    },
+    openMail() {
+      window.location.href = 'mailto:contact@tompastor.fr'
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import './IntroPart.scss';
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <HeaderMenu />
+  <div class="page">
+    <div class="header"><HeaderMenu /></div>
+    <div class="content"><IntroPart /></div>
+  </div>
 </template>
 
 <script>
@@ -9,8 +12,22 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
 body {
   margin: 0;
   background: black;
+  color: white;
+  font-family: Roboto, sans-serif;
+}
+
+.content {
+  padding: 0 8%;
+}
+
+@media screen and (max-width: 800px) {
+  .content {
+    padding: 0 5%;
+  }
 }
 </style>
