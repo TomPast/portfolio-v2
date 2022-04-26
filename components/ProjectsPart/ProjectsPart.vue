1da9b4160a57ba80586fd8fcaddcae402bde2c5f
Tom <tom.pastor.etu@univ-lemans.fr>
2022-04-26 23:11:37 +0200

    feat: setup projects part

---
+++ components/ProjectsPart/ProjectsPart.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="projects-content">
+    <div class="col-1">
+      <div class="col-header">
+        <h2 class="section-title">Some of my work.</h2>
+        <p>Here's some of my projects that I have worked on.</p>
+        <a href="">Explore more →</a>
+      </div>
+
+      <BaseCard />
+    </div>
+    <div class="col-2">
+      <BaseCard />
+      <BaseCard />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectsPart',
+  components: {},
+}
+</script>
+
+<style lang="scss" scoped>
+@import './ProjectsPart.scss';
+</style>
