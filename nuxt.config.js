5b95afb96f1fb3be1c5cd767c4f65d09c081e383
Tom <tom.pastor.etu@univ-lemans.fr>
2022-05-03 19:43:10 +0200

    fix(font): fix font not loaded on server

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,13 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['~/plugins/contentful', '~/plugins/aos.client.js'],
 
+  googleFonts: {
+    families: {
+      // a simple name
+      Roboto: true,
+    }
+  },
+
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
@@ -39,6 +46,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+    '@nuxtjs/google-fonts'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
